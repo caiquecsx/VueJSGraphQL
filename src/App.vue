46871594b948f9dfedf57a3b867c20d020f66256
caiquecsx <caique.c.s@outlook.com>
2018-10-25 17:32:38 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if="users && users.length">
+      <li v-for="user of users" v-bind:key="user.id">
+        {{user.email}}
+      </li>
+    </ul>
+    <button @click="postUser()">Add User</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const axios = require("axios");
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  data() {
+    return {
+      users: [],
+      errors: []
+    };
+  },
+  created() {this.getUsers()},
+  methods: {
+    postUser() {
+      axios
+        .post("http://localhost:3000/api", {
+          query: `
+            mutation{
+                createUser(input: {
+                  name: "Caique"
+                  email: "caique.c.s@outlook.com"
+                  password: "12345"
+                }) {
+                id
+                name
+                email
+                password
+              }
+            }
+          `
+        })
+        .then(() => {
+          this.getUsers();
+        })
+        .catch(err => {
+          console.log(err)
+        });
+    },
+
+    getUsers() {
+      axios({
+        url: "http://localhost:3000/api",
+        method: "post",
+        data: {
+          query: `
+      {
+        getUsers{
+          email,
+          id
+        }
+      }
+      `
+        }
+      }).then(result => {
+        this.users = result.data.data.getUsers;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
